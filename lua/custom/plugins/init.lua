f512799ffb6da2b490de517af3c397ef4ee5f7d1
mouldimus <thomas@sportstore.co.uk>
2024-08-07 16:42:41 +0100

    Added ai toolkit

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,12 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+
+  'mg979/vim-visual-multi', -- Multi-line editor plugin
+
+  {
+    'Exafunction/codeium.vim',
+    event = 'BufEnter',
+  },
+}
