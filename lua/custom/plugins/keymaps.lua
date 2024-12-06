3861eb638da6677ba4bc96e571b05bcb26804b60
mouldimus <thomas@sportstore.co.uk>
2024-12-06 18:30:49 +0000

    Live Previewer

--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -8,4 +8,13 @@ return {
   -- Just there for reference
   -- vim.keymap.set('t', '<esc>q', '<C-\\><C-n>:q!<cr>', { desc = '[Q]uit window' }),
   -- NVM just found out <C-d> exits the t mode && and a second press of <C-d> closes the terminal
+
+  -- Live HTML/MD Preview
+  require('which-key').add {
+    { '<leader>p', group = '[P]review' },
+  },
+  vim.keymap.set('n', '<leader>ps', ':LivePreview start<CR>', { desc = 'Preview [s]tart' }),
+  vim.keymap.set('n', '<leader>po', ':LivePreview stop<CR>', { desc = 'Preview st[o]p' }),
+  vim.keymap.set('n', '<leader>pp', ':LivePreview pick<CR>', { desc = 'Preview [p]ick' }),
+  vim.keymap.set('n', '<leader>pt', ':Autosave toggle<CR>', { desc = '[T]oggle autosave' }),
 }
