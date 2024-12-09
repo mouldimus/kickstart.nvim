e8f0fe727f9e061ff3f542c7f8fb6ed76af39910
mouldimus <thomas@sportstore.co.uk>
2024-12-09 13:21:25 +0000

    Disabled by default

--- lua/custom/plugins/live-preview.lua
+++ lua/custom/plugins/live-preview.lua
@@ -1,7 +1,15 @@
 return {
   'brianhuster/live-preview.nvim',
   dependencies = {
-    'brianhuster/autosave.nvim', -- Not required, but recomended for autosaving and sync scrolling
+    {
+      'brianhuster/autosave.nvim', -- Not required, but recomended for autosaving and sync scrolling
+      event = 'InsertEnter',
+      config = function()
+        require('autosave').setup {
+          enabled = false,
+        }
+      end,
+    },
 
     -- You can choose one of the following pickers
     'nvim-telescope/telescope.nvim',
