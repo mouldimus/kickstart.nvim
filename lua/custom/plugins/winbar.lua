fff2a3e1d87d79d9cfe5f3350f8eff1cfa74f54d
mouldimus <thomas@sportstore.co.uk>
2025-01-24 13:57:17 +0000

    Added documentation

--- lua/custom/plugins/winbar.lua
+++ lua/custom/plugins/winbar.lua
@@ -1,11 +1,13 @@
 local navic = require 'nvim-navic'
 
 local code_context = function()
+  -- disable winbar on certain filetypes
   local ft = vim.bo.filetype
   if ft == 'TelescopePrompt' or ft == 'TelescopeResults' then
     return
   end
 
+  -- if navic available get location
   if navic.is_available() then
     local location = navic.get_location()
     return location ~= '' and location or ' '
@@ -16,11 +18,13 @@ end
 
 vim.api.nvim_create_autocmd({ 'BufWinEnter', 'CursorMoved', 'WinResized', 'WinEnter', 'WinNew' }, {
   callback = function()
+    -- disable winbar on certain filetypes
     local ft = vim.bo.filetype
     if ft == 'TelescopePrompt' or ft == 'TelescopeResults' then
       return
     end
 
+    -- set window bar
     vim.opt.winbar = code_context()
   end,
 })
