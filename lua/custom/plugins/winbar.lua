971340b09bf7bdea16a98550ab44c6f00c984995
mouldimus <thomas@sportstore.co.uk>
2025-01-24 16:46:38 +0000

    Bug fix: removed lag between events

--- lua/custom/plugins/winbar.lua
+++ lua/custom/plugins/winbar.lua
@@ -1,31 +1,34 @@
 local navic = require 'nvim-navic'
+local ft = vim.bo.filetype
+local exFts = { 'TelescopePrompt', 'TelescopeResults' }
 
 local code_context = function()
   -- disable winbar on certain filetypes
-  local ft = vim.bo.filetype
-  if ft == 'TelescopePrompt' or ft == 'TelescopeResults' then
-    return
+  if ft == vim.tbl_contains(exFts, ft) then
+    return ' '
   end
 
   -- if navic available get location
   if navic.is_available() then
-    local location = navic.get_location()
+    local location = navic.get_location() -- '{} something > asd' or ''
     return location ~= '' and location or ' '
   else
     return ' '
   end
 end
 
-vim.api.nvim_create_autocmd({ 'BufWinEnter', 'CursorMoved', 'WinResized', 'WinEnter', 'WinNew' }, {
+vim.api.nvim_create_autocmd({ 'BufWinEnter', 'BufEnter', 'CursorMoved', 'WinResized', 'WinEnter', 'WinNew' }, {
   callback = function()
-    -- disable winbar on certain filetypes
-    local ft = vim.bo.filetype
-    if ft == 'TelescopePrompt' or ft == 'TelescopeResults' then
-      return
-    end
-
-    -- set window bar
-    vim.opt.winbar = code_context()
+    -- call code_context after event has been processed
+    vim.schedule(function()
+      -- disable winbar on certain filetypes
+      if ft == vim.tbl_contains(exFts, ft) then
+        return
+      else
+        -- set window bar
+        vim.opt.winbar = ' ' .. code_context()
+      end
+    end)
   end,
 })
 
